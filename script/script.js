8b30db62f980b6f3c676f5ae34bb037f1474086e
dawn_scroll <dibyomails007gmail.com>
2016-10-10 21:45:03 +0530

    not working

--- script/script.js
+++ script/script.js
@@ -7,14 +7,61 @@ $(function() {
 
       $("#game-screen").fadeToggle(300);
   });
-
+        var count = 0 ;
 
 
 
      $(window).keypress(function() {
          // jump function -----------
 
-     $("#dog").css("animation", "jump 0.5s linear both");
+        var doggy = $("#dog").position();
+        var obs = $(".pipe").position();
+
+         if (doggy.right-obs.left >= 0) {
+
+           // success jump
+           $("#dog").css("animation", "jump 0.5s linear both");
+
+          setTimeout(
+              function() {
+
+               $("#dog").css("animation", "none");
+               $(".pipe").remove();
+
+             }, 500);
+
+             count++;
+
+           }
+        else if (doggy.right-obs.left < 0) {
+
+          // false jump
+
+          $("#dog").css("animation", "out 0.1s linear forwards");
+
+          // stop whole animation
+
+          //cloud
+
+            $("#back").css("animation", "none");
+            $("#back1").css("animation", "none");
+            $("#back2").css("animation", "none");
+            $("#back3").css("animation", "none");
+            $("#back4").css("animation", "none");
+
+          //ground
+
+            $("#ground").css("animation", "none");
+
+
+
+          // pop-up of game over -
+
+          alert("your game is over and your point is : "+ count);
+
+        }
+
+  /*   $("#dog").css("animation", "jump 0.5s linear both");
 
     setTimeout(
         function() {
@@ -23,16 +70,9 @@ $(function() {
 
        }, 500);
 
+       */console.log(doggy.right);
 
-     });
+  });
 
-     // for obstacles .....
-
-      var pipeco = $("#pipe").position().left();
-    if ($ ("#dog").position().right() < pipeco+"100px")
-        {
-          $("pipe").remove();
-        }
-
-
+console.log(doggy.right);
 });
