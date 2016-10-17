779af339eb54ac4dcf2d26489e6280e50580d2d0
dawn_scroll <dibyomails007gmail.com>
2016-10-17 21:39:34 +0530

    dog position ok .. pipe position not working

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 $(function() {
     $("#game-screen").hide();
     $("#start-game").on("click", function() {
-        $("#game-screen").fadeToggle(300);
+        $("#game-screen").fadeIn(300);
     });
     var count = 0;
     $(window).keypress(function() {
@@ -9,9 +9,11 @@ $(function() {
         console.log("keypressed");
         var doggy = $("#dog").position();
         var obs = $(".pipe").position();
-        console.log("doggy's position :"+doggy.right);
+        var dogright = doggy.left + $("#dog").width();
+
+        console.log("doggy's position :"+ dogright);
         console.log("Obsticle's position :"+obs.left);
-        if (doggy.right - obs.left >= 0) {
+        if (dogright - obs.left >= 0) {
             // success jump
             $("#dog").css("animation", "jump 0.5s linear both");
             setTimeout(
@@ -20,7 +22,7 @@ $(function() {
                     $(".pipe").remove();
                 }, 500);
             count++;
-        } else if (doggy.right - obs.left < 0) {
+        } else if (dogright - obs.left < 0) {
             // false jump
             $("#dog").css("animation", "out 0.1s linear forwards");
             // stop whole animation
@@ -33,12 +35,15 @@ $(function() {
             //ground
             $("#ground").css("animation", "none");
             // pop-up of game over -
-            alert("your game is over and your point is : " + count);
+           alert("your game is over and your point is : " + count);
         }
+
+
         $("#dog").css("animation", "jump 0.5s linear both");
         setTimeout(
             function() {
                 $("#dog").css("animation", "none");
             }, 500);
+
     });
 });
